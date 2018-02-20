6111284cdac44c2d71c49351dc3355e83ced1f76
SchreinerK <ks@ksware.de>
2018-02-20 11:21:56 -0800

    TAG increment version

--- DemoApp/Properties/AssemblyInfo.cs
+++ DemoApp/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("31cabd43-96ec-4441-a3f2-75879f8916dc")]
 
-[assembly: AssemblyVersion("0.1.35")]
-[assembly: AssemblyFileVersion("0.1.35")]
+[assembly: AssemblyVersion("0.1.36")]
+[assembly: AssemblyFileVersion("0.1.36")]
