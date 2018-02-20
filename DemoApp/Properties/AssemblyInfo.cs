7659cec25966adaeac6c87ccfdaf0937f0c35523
SchreinerK <ks@ksware.de>
2018-02-20 11:01:48 -0800

    increment version

--- DemoApp/Properties/AssemblyInfo.cs
+++ DemoApp/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("31cabd43-96ec-4441-a3f2-75879f8916dc")]
 
-[assembly: AssemblyVersion("0.1.27")]
-[assembly: AssemblyFileVersion("0.1.27")]
+[assembly: AssemblyVersion("0.1.28")]
+[assembly: AssemblyFileVersion("0.1.28")]
