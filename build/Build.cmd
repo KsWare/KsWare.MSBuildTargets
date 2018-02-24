06e5c6ee8b4f175c430ae99f03b6288e99231a2c
SchreinerK <ks@ksware.de>
2018-02-24 13:47:31 +0100

    .

--- build/Build.cmd
+++ build/Build.cmd
@@ -1,46 +1,40 @@
 @echo Off
 SETLOCAL
+REM set current path to repository root
+%~d0
+cd %~dp0..
+echo %CD%
+
 set ConfigurationName=%1
 
 if "%ConfigurationName%" == "" (
    set ConfigurationName=Debug
 )
 
-set ProjectPath=%~dp0KsWare.MSBuildTargets.csproj
-set TargetPath=%~dp0bin\%ConfigurationName%\KsWare.MSBuildTargets.exe
-
-REM Dev10 and Dev11 msbuild path
-set nugetmsbuildpath="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild"
-
-REM Dev12 msbuild path
-set nugetmsbuildpathtmp="%ProgramFiles%\MSBuild\12.0\bin\msbuild"
-if exist %nugetmsbuildpathtmp% set nugetmsbuildpath=%nugetmsbuildpathtmp%
-set nugetmsbuildpathtmp="%ProgramFiles(x86)%\MSBuild\12.0\bin\msbuild"
-if exist %nugetmsbuildpathtmp% set nugetmsbuildpath=%nugetmsbuildpathtmp%
-
-REM Dev14 msbuild path
-set nugetmsbuildpathtmp="%ProgramFiles%\MSBuild\14.0\bin\msbuild"
-if exist %nugetmsbuildpathtmp% set nugetmsbuildpath=%nugetmsbuildpathtmp%
-set nugetmsbuildpathtmp="%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild"
-if exist %nugetmsbuildpathtmp% set nugetmsbuildpath=%nugetmsbuildpathtmp%
+set ProjectPath="%CD%\src\KsWare.MSBuildTargets\KsWare.MSBuildTargets.csproj"
+set TargetPath="%CD%\src\KsWare.MSBuildTargets\bin\%ConfigurationName%\KsWare.MSBuildTargets.exe"
 
 REM Dev15 msbuild path
-set nugetmsbuildpathtmp="%ProgramFiles%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild"
-if exist %nugetmsbuildpathtmp% set nugetmsbuildpath=%nugetmsbuildpathtmp%
-set nugetmsbuildpathtmp="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild"
-if exist %nugetmsbuildpathtmp% set nugetmsbuildpath=%nugetmsbuildpathtmp%
+::TODO use vswhere.exe 
+set msbuildtmp="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+if exist %msbuildtmp% set msbuild=%msbuildtmp%
+set path=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
+
+::TODO configure better path
+set nuget="%~dp0src\KsWare.MSBuildTargets\bin\%ConfigurationName%\nuget.exe"
 
 set EnableNuGetPackageRestore=true
-::TODO enable restore and fix errors
-::bin\debug\nuget.exe restore
-%nugetmsbuildpath% KsWare.MSBuildTargets.sln /p:Configuration="%ConfigurationName%" /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Detailed /nr:false 
+::TODO %nuget% restore src
 
-:: Create package 
+:: TODO the fucking shit would't work in this cmd-file!
+msbuild src\KsWare.MSBuildTargets.sln /p:Configuration="%ConfigurationName%" /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Detailed /nr:false
+pause
+REM Create package 
 if "%ConfigurationName%" == "Debug" (
-	bin\%ConfigurationName%\KsWare.MSBuildTargets.exe -pp "%ProjectPath%" -cn "%ConfigurationName%" -pn "Any CPU" -tp "%TargetPath%"
+	%TargetPath% -pp %ProjectPath% -cn "%ConfigurationName%" -pn "Any CPU" -tp %TargetPath%
 )
 if "%ConfigurationName%" == "Release" (
-	bin\release\nuget.exe pack KsWare.MSBuildTargets.nuspec
+	%nuget% pack %ProjectPath% -tool
 )
 ENDLOCAL
 pause
