ecffe990307a9223ce9e8a53cd0002add9c6e0f9
SchreinerK <ks@ksware.de>
2018-02-24 11:14:53 -0800

    [skip ci] increment version %appveyor_build_version%

--- src/KsWare.MSBuildTargets.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.MSBuildTargets.Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("267b3f8d-b100-4084-b030-31f87cbe01f8")]
 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.1.71")]
+[assembly: AssemblyVersion("0.1.71")]
+[assembly: AssemblyFileVersion("0.1.71")]
