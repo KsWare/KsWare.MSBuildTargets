abc33a83eb2c59afe896be71225c7b43ecdac544
SchreinerK <ks@ksware.de>
2018-02-24 12:59:38 -0800

    [skip ci] increment version 0.1.78

--- src/KsWare.MSBuildTargets/Properties/AssemblyInfo.cs
+++ src/KsWare.MSBuildTargets/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a8d1f8b9-3d84-46c0-b347-1fc7960ac8a2")]
 
-[assembly: AssemblyVersion("0.1.76")]
+[assembly: AssemblyVersion("0.1.78")]
 [assembly: InternalsVisibleTo("KsWare.MSBuildTargets.Tests")]
