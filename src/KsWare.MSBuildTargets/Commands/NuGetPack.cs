7b974c11debd7bca9b96856b4681cd862de0d988
SchreinerK <ks@ksware.de>
2019-10-06 16:13:13 +0200

    update nuget packages. targetframework 4.7.2

--- src/KsWare.MSBuildTargets/Commands/NuGetPack.cs
+++ src/KsWare.MSBuildTargets/Commands/NuGetPack.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml;
 using System.Xml.Serialization;
 using KsWare.MSBuildTargets.Internal;
-using NuGet.Versioning;
 
 namespace KsWare.MSBuildTargets.Commands {
 
